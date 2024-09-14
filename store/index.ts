7c7f4b329bea02b1ae11c21e0419213fe93e89b8
Denitsa Slavkova <deni9309@gmail.com>
2024-09-14 19:35:27 +0300

    Implement driver store; Render a map with nearby drivers

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 
-import { LocationStore } from '@/types/type'
+import { DriverStore, LocationStore, MarkerData } from '@/types/type'
 
 export const useLocationStore = create<LocationStore>((set) => ({
   userAddress: null,
@@ -23,4 +23,16 @@ export const useLocationStore = create<LocationStore>((set) => ({
       destinationAddress: address,
     }))
   },
+}))
+
+export const useDriverStore = create<DriverStore>((set) => ({
+  drivers: [] as MarkerData[],
+  selectedDriver: null,
+
+  setSelectedDriver: (driverId: number) =>
+    set(() => ({ selectedDriver: driverId })),
+
+  setDrivers: (drivers: MarkerData[]) => set(() => ({ drivers: drivers })),
+
+  clearSelectedDriver: () => set(() => ({ selectedDriver: null })),
 }))
