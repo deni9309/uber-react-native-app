5d8cd0e2a43f630e7bdef4504d50c7b1a334b002
Denitsa Slavkova <deni9309@gmail.com>
2024-09-20 18:30:56 +0300

    Finalize

--- app/(api)/(stripe)/create+api.ts
+++ app/(api)/(stripe)/create+api.ts
@@ -28,7 +28,7 @@ export async function POST(request: Request) {
 
   const ephemeralKey = await stripe.ephemeralKeys.create(
     { customer: customer.id },
-    { apiVersion: '2024-09-17' },
+    { apiVersion: '2024-06-20' },
   )
 
   const paymentIntent = await stripe.paymentIntents.create({
